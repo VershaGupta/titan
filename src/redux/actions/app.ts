8210c63c9be46a40f67c1524fd28d53d045edf73
Versha <versha.gupta@ad.loginradius.net>
2020-09-03 18:42:49 +0530

    Initial code

---
+++ src/redux/actions/app.ts
@@ -0,0 +1,26 @@
+import { createAction } from ".";
+import { IAPIError } from "../../schema";
+
+export const appConst = {
+  GET_APPS: "GET_APPS",
+  CREATE_APP: "CREATE_APP",
+};
+
+export interface IApp extends IAPIError {
+  customerid: string;
+  appID: string;
+  appName: string;
+  Customeremailid: string;
+
+  Created_date: string;
+  updated_date: string;
+}
+
+export const getAppsAction = createAction<IApp[]>(appConst.GET_APPS);
+
+export interface IAddAppArgs {
+  customerId: string;
+}
+export const addAppAction = createAction<IApp, IAddAppArgs>(
+  appConst.CREATE_APP
+);
